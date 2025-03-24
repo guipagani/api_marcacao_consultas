146b221a2b5d9c4336966a40d2b70aa099db2772
Guilherme Pagani <paganigg>
2025-03-24 15:46:27 -0300

    Aprimorando o Modelo de Consulta.

--- src/main/java/com/fiap/ecb/api_marcacao_consultas/model/Consulta.java
+++ src/main/java/com/fiap/ecb/api_marcacao_consultas/model/Consulta.java
@@ -9,9 +9,20 @@ public class Consulta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime dataHora;
+
     private String especialidade;
+
     @ManyToOne
     @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
+    private Usuario usuario; // Paciente
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Usuario medico; // Médico
+
+    private String status; // AGENDADA, REALIZADA, CANCELADA
+
+    private String observacao; // Observações sobre a consulta
 }
